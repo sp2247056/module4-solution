89e3aa7d38b7a5a535edcc885ad4c5eb44c675e7
Shubham Pandey <65395538+sp2247056@users.noreply.github.com>
2020-06-12 02:32:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+   var names = ["Utkarsh", "Jatin", "Jnelia", "Jassu", "Prince", "raj", "raushan", "simran", "sunaina", "kanika"];
    for (var name in names) {
       var firstLetter = names[name].charAt(0).toLowerCase();
        if (firstLetter === 'j') {
